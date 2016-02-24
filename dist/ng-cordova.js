48c171b68e91a181b1e74d1a1325d3c6747d0b74
Balint Vekerdy <balint@dangerfarms.com>
2016-02-24 16:12:58 +0000

    Update oauth

--- dist/ng-cordova.js
+++ dist/ng-cordova.js
@@ -7781,7 +7781,7 @@ angular.module("oauth.utils", [])
                   "org.apache.cordova.inappbrowser"
                 ];
                 var matchFunction = function(plugin) {
-                    return inAppBrowserNames.some(function(name) {return plugin.pluginId == name;});
+                    return inAppBrowserNames.some(function(name) {return plugin.id == name;});
                 };
                 return cordovaPluginList.some(matchFunction);
             },
@@ -11077,7 +11077,7 @@ angular.module("oauth.utils", [])
                     "org.apache.cordova.inappbrowser"
                 ];
                 var matchFunction = function(plugin) {
-                    return inAppBrowserNames.some(function(name) {return plugin.pluginId == name;});
+                    return inAppBrowserNames.some(function(name) {return plugin.id == name;});
                 };
                 return cordovaPluginList.some(matchFunction);
             },
